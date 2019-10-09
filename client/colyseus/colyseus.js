98d6fe6c848b7636298bb1232b6deea2e425bcec
Fabricio dos Santos <fabricio.antunes@bludata.com.br>
2019-10-09 13:12:08 -0300

    heroku

--- client/colyseus/colyseus.js
+++ client/colyseus/colyseus.js
@@ -1,5 +1,5 @@
 import * as Colyseus from "colyseus.js";
 /*  Colyseus */
-var client = new Colyseus.Client(process.env.ColyseysUrl);
+var client = new Colyseus.Client(process.env.ColyseysUrl || 'ws://192.168.0.141:2567');
 
  export default client;
