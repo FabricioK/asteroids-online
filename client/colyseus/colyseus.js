fcac54786960bcf9bf1540cd5c2934a1d56cfc76
Fabricio dos Santos <fabricio.antunes@bludata.com.br>
2019-10-09 13:30:37 -0300

    heroku

--- client/colyseus/colyseus.js
+++ client/colyseus/colyseus.js
@@ -1,5 +1,5 @@
 import * as Colyseus from "colyseus.js";
 /*  Colyseus */
-var client = new Colyseus.Client(process.env.ColyseysUrl || 'ws://asteroids-war.herokuapp.com:2567');
+var client = new Colyseus.Client(process.env.ColyseysUrl || 'wss://asteroids-war.herokuapp.com:2567');
 
  export default client;
